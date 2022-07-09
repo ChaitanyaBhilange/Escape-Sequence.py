87627eccae3751fe3133c344d9c3bc8151c31c0d
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-08 23:02:29 -0700

    Add files via upload

---
+++ Escape sequence.py
@@ -0,0 +1,5 @@
+#wpp to perform escape perform
+#\n->new line
+#\t->
+st1="Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.n\ttwinkle, twinkle, little star, \n\tHow I wonder what you are" 
+print(st1)
